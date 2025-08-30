9e81ae2ff5ea69d61247859655ea64579b38f04c
Sachin Sen <sachinsen1920@gmail.com>
2025-08-31 00:27:17 +0530

    update cloudinary

--- saas-pro/app/api/video-upload/route.ts
+++ saas-pro/app/api/video-upload/route.ts
@@ -1,4 +1,4 @@
-import { v2 as cloudinary, UploadStream } from "cloudinary";
+import { v2 as cloudinary } from "cloudinary";
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse, NextRequest } from "next/server";
 import { PrismaClient } from "@/generated/prisma";
